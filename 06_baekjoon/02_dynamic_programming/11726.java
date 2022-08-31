bb4328610cb3065822d11b4e820b7d4cf91cf3ce
suebin <suuebin@gmail.com>
2022-09-01 07:20:14 +0900

    2×n 타일링

--- 06_baekjoon/02_dynamic_programming/11726.java
+++ 06_baekjoon/02_dynamic_programming/11726.java
@@ -14,8 +14,9 @@ public static void main(String[] args) throws IOException {
     System.out.println(dp[N]);
     
     /*
-     * mod 연산을 한 결과값을 출력해야 할 때에는 반드시 연산할 때마다 mod 연산을 해주어야 한다. 계속 숫자를 더하고 마지막 출력 시에만
-     * mod 연산을 해줄 경우 Integer.MAX_VALUE를 넘어 Overflow가 발생하기 때문에 잘못된 값이 출력될 수 있다.
+     * mod 연산을 한 결과값을 출력해야 할 때에는 반드시 연산할 때마다 mod 연산을 해주어야 한다. 
+     * 계속 숫자를 더하고 마지막 출력 시에만 mod 연산을 해줄 경우
+     * Integer.MAX_VALUE를 넘어 Overflow가 발생하기 때문에 잘못된 값이 출력될 수 있다.
      * (참고: https://girawhale.tistory.com/33)
      */
     }
