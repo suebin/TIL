35fc68bde3a45f6f878f0725507798147a4563b4
suebin <suuebin@gmail.com>
2023-06-28 15:12:41 +0900

    🥈 silver5: 팩토리얼 0의 개수

--- 04_algorithm/boj/01_math/1676.java
+++ 04_algorithm/boj/01_math/1676.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
