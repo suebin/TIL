16cf76b4e59736a8dda7db0a5171a9a43e02d454
suebin <suuebin@gmail.com>
2023-06-28 15:16:02 +0900

    🥈 silver2: 골드바흐 파티션

--- 04_algorithm/boj/01_math/17103.java
+++ 04_algorithm/boj/01_math/17103.java
@@ -18,7 +18,6 @@ public class Main {
 		getPrime();
 		
 		for (int i=0; i<T; i++) {
-			
 			int N = Integer.parseInt(br.readLine());
 			
 			int partition1 = N / 2;
@@ -46,7 +45,6 @@ public class Main {
 		prime[0] = prime[1] = true;
 		
 		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
-
 			if (prime[i]) {
 				continue;
 			}
@@ -55,6 +53,5 @@ public class Main {
 				prime[j] = true;
 			}
 		}
-		
 	}
 }
