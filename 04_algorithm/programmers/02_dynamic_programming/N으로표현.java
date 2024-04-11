28878b031ff44a0faa8c992a14500d8de7cb130b
suebin <suuebin@gmail.com>
2024-04-11 14:42:06 +0900

    Level3: N으로 표현

--- 04_algorithm/programmers/02_dynamic_programming/N으로표현.java
+++ 04_algorithm/programmers/02_dynamic_programming/N으로표현.java
@@ -1,43 +1,38 @@
 import java.util.*;
 
 class Solution {
+    private static Set<Integer>[] set;
+
     public int solution(int N, int number) {
-        List<Set<Integer>> setList = new ArrayList<>();
+        if (N == number) return 1;
 
-        for (int i = 0; i < 9; i++) {
-            setList.add(new HashSet<>());
+        set = new HashSet[9];
+        for (int i = 1; i <= 8; i++) {
+            set[i] = new HashSet<>();
         }
-        setList.get(1).add(N);
-        if (number == N) {
-            return 1;
-        }
+        set[1].add(N);
 
-        for (int i = 2; i < 9; i++) {
-            for (int j = 1; j <= i / 2; j++) {
-                operate(setList.get(i), setList.get(i - j), setList.get(j));
-                operate(setList.get(i), setList.get(j), setList.get(i - j));
-            }
-            String n = Integer.toString(N);
-            setList.get(i).add(Integer.parseInt(n.repeat(i)));
+        String num = Integer.toString(N);
 
-            if (setList.get(i).contains(number)) {
-                return i;
+        for (int i = 2; i <= 8; i++) {
+            for (int j = 1; j < i; j++) {
+                operate(i, j, i - j);
             }
+
+            set[i].add(Integer.parseInt(num.repeat(i)));
+            if (set[i].contains(number)) return i;
         }
 
         return -1;
     }
 
-    static void operate(Set<Integer> newSet, Set<Integer> set, Set<Integer> otherSet) {
-        for (int num : set) {
-            for (int otherNum : otherSet) {
-                newSet.add(num + otherNum);
-                newSet.add(num - otherNum);
-                newSet.add(num * otherNum);
-
-                if (otherNum != 0) {
-                    newSet.add(num / otherNum);
-                }
+    private static void operate(int cur, int n1, int n2) {
+        for (int x : set[n1]) {
+            for (int y : set[n2]) {
+                set[cur].add(x + y);
+                set[cur].add(x * y);
+                set[cur].add(x - y);
+                if (y != 0) set[cur].add(x / y);
             }
         }
     }
