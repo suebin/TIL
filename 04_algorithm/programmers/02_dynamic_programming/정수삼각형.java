72f3de15ca2c714929eaa228b784cba99886659f
suebin <suuebin@gmail.com>
2024-04-11 15:58:33 +0900

    Level3: 정수 삼각형

--- 04_algorithm/programmers/02_dynamic_programming/정수삼각형.java
+++ 04_algorithm/programmers/02_dynamic_programming/정수삼각형.java
@@ -2,21 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        int[][] dp = new int[triangle.length][triangle.length];
-
+        int len = triangle.length;
+        int[][] dp = new int[len][len];
         dp[0][0] = triangle[0][0];
-        for (int i = 1; i < triangle.length; i++) {
-            dp[i][0] = dp[i - 1][0] + triangle[i][0];
-            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
 
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
             for (int j = 1; j <= i; j++) {
                 dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
             }
         }
 
-        for (int i = 0; i < triangle.length; i++) {
-            answer = Math.max(answer, dp[triangle.length - 1][i]);
+        int answer = 0;
+        for (int i : dp[len - 1]) {
+            answer = Math.max(answer, i);
         }
 
         return answer;
